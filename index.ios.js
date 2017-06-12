d8e1d75e2a1dc59525e2d0fe1bc7ac241c884748
Lucas Bicca <lucaslbs15@gmail.com>
2017-06-11 21:51:25 -0300

    Copy code from index.android.js to index.ios.js.

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+// import a library to help create a Component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+// create a Component
+const App = () => (
+  <Header headerText={'Albums'} />
+);
+// render it to the device
+AppRegistry.registerComponent('albums', () => App);
